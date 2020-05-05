8fa04ceab1d781bfec7c7cd037f3ea8769fcfca3
kr1va <krivakriva@gmail.com>
2020-05-06 00:23:49 +0300

    urlManager fixing

--- config/web.php
+++ config/web.php
@@ -11,10 +11,12 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'defaultRoute' => 'testapp/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Gw5gi3R8hbymkbmvGnCt2OvXoQWLLH10',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+//        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+//        */
     ],
     'params' => $params,
 ];
